7a2a1622f408f573ca087c46fa05f4d630d3a38d
Farry <newfarry@126.com>
2019-11-25 21:34:19 +0800

    mdns modify

--- src/user_mdns.c
+++ src/user_mdns.c
@@ -1,4 +1,3 @@
-
 #include "appmain.h"
 #include "user_function.h"
 #include <hsf.h>
@@ -6,49 +5,33 @@
 #include <string.h>
 #include <mdns.h>
 
-#include "mdns.h"
-
-#define SYS_TXT_LEN  256
-
-static struct mdns_service mico_system_service;
-static char keyvals[SYS_TXT_LEN];
-
 void mdns_thread( void *arg );
+
+static char module_mac[21];
+
+static struct mdns_service test_service1 = {
+	.servname = module_mac,
+	.servtype = "iotdevice",
+	.domain   = "local",
+	.proto    = MDNS_PROTO_TCP,
+	.port     = 80,
+	.keyvals = "DynagteID=1234567890",
+	.kvlen = sizeof("DynagteID=1234567890")
+};
 
 USER_FUNC void user_mdns_init( void )
 {
-   	hfthread_create(mdns_thread,"mdns",1024,(void*)1,1,NULL,NULL);  //∆Ù∂Ømdnsœﬂ≥Ã
+	hfthread_create(mdns_thread,"mdnscontrol",1024,(void*)1,1,NULL,NULL);
 }
 
-/*create mdns socket*/
 void USER_FUNC mdns_thread( void *arg )
 {
-	char *val;
+	while(!hfnet_wifi_is_active())
+	{
+		msleep(50);
+	}
+	hfnet_get_mac_address(module_mac);
+	mdns_start("local", module_mac);
 
-	memset(&mico_system_service, 0x0, sizeof(struct mdns_service));
-	mico_system_service.servname = "servname";
-	mico_system_service.servtype = "easylink";
-	mico_system_service.port = 80;
-	mico_system_service.proto = MDNS_PROTO_TCP;
-
-	val = "MAC";
-	snprintf(keyvals, SYS_TXT_LEN, "MAC=%s.", val);
-	free(val);
-
-	val = "Firmware";
-	snprintf(keyvals, SYS_TXT_LEN, "%sFirmware Rev=%s.", keyvals, val);
-	free(val);
-
-	val = "Hardware";
-	snprintf(keyvals, SYS_TXT_LEN, "%sHardware Rev=%s.", keyvals, val);
-	free(val);
-
-	val = "os";
-	snprintf(keyvals, SYS_TXT_LEN, "%sMICO OS Rev=%s.", keyvals, val);
-	free(val);
-
-	mdns_set_txt_rec(&mico_system_service, keyvals);
-
-	mdns_start("start1", "start2");
-	mdns_announce_service(&mico_system_service, NULL);
+	mdns_announce_service(&test_service1, "STA");
 }
